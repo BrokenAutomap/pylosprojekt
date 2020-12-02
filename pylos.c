7dcd5854709cfd995a13322181cf9300786693d4
BrokenAutomap <sebastian.misiarz1@gmail.com>
2020-12-02 12:31:11 +0100

    ver 0.0 początek projektu

---
+++ pylos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+struct level{
+    int levelIndex;
+    int *levelPlane;
+    int *levelPlaneFlags;
+};
+
+struct level createLevel(int levelIndex)
+{
+
+};
+
+void main()
+{
+
+}
